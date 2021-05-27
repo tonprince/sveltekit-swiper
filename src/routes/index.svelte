9f02dc0703ae34b67d35a6b218ba20c42884e3a5
Stefan <tonprince@gmail.com>
2021-05-27 09:49:13 +0700

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Swiper from "swiper/esm/svelte/swiper.svelte";
+    import SwiperSlide from "swiper/esm/svelte/swiper-slide.svelte";
+    import SwiperCore, { Navigation, Pagination } from "swiper";
+    SwiperCore.use([Navigation, Pagination]);
+    import "swiper/swiper-bundle.css";
+</script>
+
+<Swiper style="width: 300px" slidesPerView={1} navigation>
+    <SwiperSlide>foo</SwiperSlide>
+    <SwiperSlide>bar</SwiperSlide>
+</Swiper>
